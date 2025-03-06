026f6276f81437e2c33c06f68dfe48041d7cf2d1
suraj patel <sp9589890205@gmail.com>
2025-03-06 10:31:11 +0530

    [Suraj] added. Bug resolved for user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
+  standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -11,11 +12,21 @@ export class AppComponent {
   title = 'Hello From BridgeLabz';
   imgUrl="../assests/images/download.png";
   url="https://www.bridgelabz.com";
-  userName : string ="Suraj";
+  userName : string ="";
+  nameError:string="";
 
   onClick(){
     console.log("Save Button is Clicked : ");
     window.open(this.url,"_blank")
 
+   }
+   onInput(): void{
+    console.log("change Event Occured!");
+    const nameRegex=RegExp(`^[A-Z]{1}[a-zA-Z\\s]{2,}$`);
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    
    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+// import { BrowserModule } from "@angular/platform-browser";
+// import { NgModule } from "@angular/core";
+// import { FormsModule } from "@angular/forms";
 
 
-import { AppComponent } from "./app.component";
+// import { AppComponent } from "./app.component";
 
-@NgModule({
-  declarations:[
-    AppComponent
-  ],
-  imports:[
-    BrowserModule,
-    FormsModule
+// @NgModule({
+//   declarations:[
+//     AppComponent
+//   ],
+//   imports:[
+//     BrowserModule,
+//     FormsModule
     
-  ],
-  providers:[],
-  bootstrap:[AppComponent]
-})
-export class AppModule {}
+//   ],
+//   providers:[],
+//   bootstrap:[AppComponent]
+// })
+// export class AppModule {}
 
